54ffe5c3b54b3a2b652b508e1f0c299e408d8af2
Jake Lee <jakelee8@icloud.com>
2017-05-19 17:15:28 -0700

    Finish RNN project

--- my_answers.py
+++ my_answers.py
@@ -1,45 +1,97 @@
+import re
+
 import numpy as np
 
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
-import keras
 
 
-# TODO: fill out the function below that transforms the input series 
-# and window-size into a set of input/output pairs for use with our RNN model
-def window_transform_series(series,window_size):
-    # containers for input/output pairs
+def window_transform_series(series, window_size):
+    """
+    Transform the input series and window-size into a set
+    of input/output pairs for use with our RNN model.
+
+    Args:
+      series: The input data series.
+      window_size: The window size.
+    Returns:
+      The input/output pairs.
+    """
+    # container for inputs
     X = []
-    y = []
 
-    # reshape each 
+    # loop over input start indexes
+    for i in range(len(series) - window_size):
+        # add sliced window to inputs container
+        X.append(series[i:i + window_size])
+
+    # outputs are the next values after the window
+    y = series[window_size:]
+
+    # reshape each
     X = np.asarray(X)
-    X.shape = (np.shape(X)[0:2])
+    X = X.reshape(np.shape(X)[:2])
     y = np.asarray(y)
-    y.shape = (len(y),1)
-    
-    return X,y
+    y = y.reshape((len(y), 1))
 
-# TODO: build an RNN to perform regression on our time series input/output data
+    return X, y
+
+
 def build_part1_RNN(step_size, window_size):
-    pass
+    """
+    Build an RNN to perform regression on our time series input/output data.
+
+    Args:
+      step_size: Number of steps per input record.
+      window_size: Size of the window on the data.
+    Returns:
+      The RNN model.
+    """
+    # Create the model.
+    model = Sequential()
+    # Add a LSTM layer with 5 hidden units.
+    model.add(LSTM(5, input_shape=(window_size, step_size)))
+    # Add a fully-connected layer with 1 hidden unit.
+    model.add(Dense(1))
+    return model
 
 
-### TODO: list all unique characters in the text and remove any non-english ones
 def clean_text(text):
-    # find all unique characters in the text
+    """
+    List all unique characters in the text and remove any non-english ones.
+
+    Args:
+      text: The input text.
+    Returns:
+      The cleaned text.
+    """
+    # Use a regular expression to replace non-english letters with a space.
+    text = re.sub(r'[^a-z.,\-\'\"]+', ' ', text, flags=re.IGNORECASE)
+    return text
 
 
-    # remove as many non-english characters and character sequences as you can 
+def window_transform_text(text, window_size, step_size):
+    """
+    Transform the input text and window-size into a set of
+    input/output pairs for use with our RNN model.
 
-
-### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
-def window_transform_text(text,window_size,step_size):
-    # containers for input/output pairs
+    Args:
+      text: The input text.
+      window_size: The window size.
+      step_size: The step size.
+    Returns:
+      The input/output pairs.
+    """
+    # container for inputs
     inputs = []
-    outputs = []
-    
 
-    
-    return inputs,outputs
+    # loop over input start indexes
+    for i in range(0, len(text) - window_size, step_size):
+        # add sliced window to inputs container
+        inputs.append(text[i:i + window_size])
+
+    # outputs are the next values after the window
+    outputs = text[window_size::step_size]
+
+    return inputs, outputs
